11984f0abaa07ba1f7f9725a108277f3ff48ddd4
titanicsunkomg <61984029+titanicsunkomg@users.noreply.github.com>
2020-07-24 01:27:49 +0800

    Add files via upload

---
+++ brownian/Ball.js
@@ -0,0 +1,105 @@
+﻿var maxTraceLength = 700;
+
+class Ball {
+    constructor(identity, x, y, w, vx, vy, m) {
+        this.identity = identity;
+        this.position = createVector(x, y);
+        if (this.identity == "dust") {
+            this.trace = [];
+            if (drawTrace == true) {
+                this.trace.push(this.position.copy());
+            }
+        }
+        this.w = w;
+        this.velocity = createVector(vx, vy);
+        this.m = m;
+        this.colorR = random(255);
+        this.colorG = random(255);
+        this.colorB = random(255);
+    }
+
+    move() {
+        this.position.add(this.velocity);
+        if (this.identity == "dust" && drawTrace == true) {
+            this.trace.push(this.position.copy());
+            if (this.trace.length > maxTraceLength) {
+                this.trace.shift();
+            }
+        }
+    }
+
+    display() {
+        if (this.identity == "container") {
+            fill(255, 255, 255);
+            ellipse(this.position.x, this.position.y, this.w);
+        } else if (this.identity == "dust") {
+            //drawing trace path
+            noFill();
+            if (this.trace.length > 0) {
+                for (let i = 1; i < this.trace.length; i++) {
+                    beginShape();
+                    stroke(map(i + maxTraceLength - this.trace.length, 0, maxTraceLength, 255, 50), 90);
+                    strokeWeight(1);
+                    vertex(this.trace[i - 1].x, this.trace[i - 1].y);
+                    vertex(this.trace[i].x, this.trace[i].y);
+                    endShape();
+                }
+            }
+            noStroke();
+
+            fill(139, 69, 19);
+            ellipse(this.position.x, this.position.y, this.w);
+        } else {
+            if (visibleBool.checked == true) {
+                fill(this.colorR, this.colorG, this.colorB);
+                ellipse(this.position.x, this.position.y, this.w);
+            } else {
+                noStroke();
+                fill(0, 0);
+                ellipse(this.position.x, this.position.y, this.w);
+            }
+        }
+    }
+
+    checkWallCollision(container) {
+        let thisnextpos = this.position.copy().add(this.velocity.copy());
+        let d = thisnextpos.dist(container.position);
+        if (d > (container.w / 2 - this.w / 2)) {
+            this.velocity.mult(-1);
+        }
+    }
+
+    checkBallCollision(other) {
+        let thisnextpos = this.position.copy().add(this.velocity.copy());
+        let othernextpos = other.position.copy().add(other.velocity.copy());
+        let d = thisnextpos.dist(othernextpos);
+        if (d < (this.w / 2 + other.w / 2)) {
+            let tempthisvel1 = this.velocity.copy().mult((this.m - other.m) / (this.m + other.m));
+            let tempthisvel2 = other.velocity.copy().mult(2 * other.m / (this.m + other.m));
+            let tempthisvel = tempthisvel1.add(tempthisvel2);
+            let tempothervel1 = this.velocity.copy().mult(2 * this.m / (this.m + other.m));
+            let tempothervel2 = other.velocity.copy().mult((other.m - this.m) / (this.m + other.m));
+            let tempothervel = tempothervel1.add(tempothervel2);
+            this.velocity = tempthisvel;
+            other.velocity = tempothervel;
+        }
+    }
+}
+
+class Flock {
+    constructor() {
+        this.list = [];
+    }
+
+    update(container) {
+        for (let i in this.list) {
+            for (var j = 1; j < (this.list.length - Number(i)); j++) {
+                this.list[i].checkBallCollision(this.list[Number(i) + j]);
+            }
+            this.list[i].checkWallCollision(container);
+            this.list[i].move();
+            this.list[i].display();
+        }
+    }
+
+}
---
+++ brownian/sketch.js
@@ -0,0 +1,108 @@
+﻿var balls;
+var container;
+var ballNum = 300;
+var dustNum = 1;
+var dustMass = 75;
+var dustSize = 20;
+var vLimit = 10;
+var visibleBool;
+var drawTrace = false;
+
+function setup() {
+    var canvas = createCanvas(800, 800);
+    // Move the canvas so it’s inside our <div id="sketch-holder">.
+    canvas.parent('sketch-holder');
+
+    $(".airvalue").text(ballNum);
+    $("#airslider").on("input", function () {
+        $(".airvalue").text(this.value);
+        ballNum = this.value;
+        reset();
+    });
+    balls = new Flock();
+    container = new Ball("container", 400, 400, 800, 0, 0, 1);
+    visibleBool = document.getElementById("visible");
+
+    for (i = 0; i < dustNum; i++) {
+        x = 400 + 50 * sin(2 * PI / dustNum * i);
+        y = 400 + 50 * cos(2 * PI / dustNum * i);
+        balls.list.push(new Ball("dust", x, y, dustSize, 0, 0, dustMass));
+    }
+
+    for (i = 0; i < ballNum; i++) {
+        let m = 1;
+        let w = 5 * m;
+        let x;
+        let y;
+        if (i < (ballNum / 2)) {
+            x = 400 + 300 * sin(2 * PI / (ballNum / 2) * i);
+            y = 400 + 300 * cos(2 * PI / (ballNum / 2) * i);
+        } else {
+            x = 400 + 200 * sin(2 * PI / (ballNum / 2) * i);
+            y = 400 + 200 * cos(2 * PI / (ballNum / 2) * i);
+        }
+        let vx = random(-vLimit, vLimit);
+        let vy = random(-vLimit, vLimit);
+        balls.list.push(new Ball("air", x, y, w, vx, vy, m));
+    }
+}
+
+function draw() {
+    background(0);
+    container.display();
+    balls.update(container);
+}
+
+function brownian() {
+    dustNum = 1;
+    reset();
+}
+
+function diffusion() {
+    dustNum = 10;
+    reset();
+}
+
+function traceClick() {
+    drawTrace = document.getElementById("trace").checked;
+    for (let i = 0; i < dustNum; i++) {
+        balls.list[i].trace = [];
+    }
+}
+
+function reset() {
+    balls = new Flock();
+    container = new Ball("container", 400, 400, 800, 0, 0, 1);
+    visibleBool = document.getElementById("visible");
+
+    for (i = 0; i < dustNum; i++) {
+        x = 400 + 50 * sin(2 * PI / dustNum * i);
+        y = 400 + 50 * cos(2 * PI / dustNum * i);
+        balls.list.push(new Ball("dust", x, y, dustSize, 0, 0, dustMass));
+    }
+
+    for (i = 0; i < ballNum; i++) {
+        let m = 1;
+        let w = 5 * m;
+        let x;
+        let y;
+        if (i < (ballNum / 2)) {
+            x = 400 + 300 * sin(2 * PI / (ballNum / 2) * i);
+            y = 400 + 300 * cos(2 * PI / (ballNum / 2) * i);
+        } else {
+            x = 400 + 200 * sin(2 * PI / (ballNum / 2) * i);
+            y = 400 + 200 * cos(2 * PI / (ballNum / 2) * i);
+        }
+        let vx = random(-vLimit, vLimit);
+        let vy = random(-vLimit, vLimit);
+        balls.list.push(new Ball("air", x, y, w, vx, vy, m));
+    }
+}
+
+function slowmo() {
+    if (document.getElementById("slowmo").checked == false) {
+        frameRate(60);
+    } else {
+        frameRate(15);
+    }
+}
