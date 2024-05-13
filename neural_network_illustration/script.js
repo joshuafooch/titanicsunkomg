3e76237fcf902d37611dd4e56f3726d30642e70f
titanicsunkomg <61984029+titanicsunkomg@users.noreply.github.com>
2024-05-13 17:19:11 +0800

    Add files via upload

--- neural_network_illustration/script.js
+++ neural_network_illustration/script.js
@@ -12,7 +12,7 @@ for(let i=0; i<28; i++){
 }
 centroidMap = tf.tensor(centroidMap);
 
-// instantiate model and epoch counter
+// instantiate models
 const trainedModel = await tf.loadLayersModel("trained-model/model.json"); // pre-trained 3-layer NN on MNIST digits
 const untrainedModel = await tf.loadLayersModel("untrained-model/model.json"); // untrained 3-layer NN on MNIST digits with random weights
 
@@ -28,19 +28,81 @@ export function recognize(pixels) {
   pixelArray = tf.image.resizeBilinear(pixelArray, [28, 28]); //resize to (28, 28, 1)
 
   // centralise image on its centroid
-  let centroidProduct = tf.sum(tf.sum(tf.mul(pixelArray, centroidMap), 1), 0);
-  let pixelSum = tf.sum(tf.sum(pixelArray, 1), 0);
-  let centroid = tf.round(tf.div(centroidProduct, pixelSum));
-  let [centroidX, centroidY] = centroid.dataSync();
-  let [imageSliceLeft, xSize] = (centroidX < 14) ? [0, centroidX] : [2 * centroidX - 28, 28 - centroidX];
-  let [imageSliceTop, ySize] = (centroidY < 14) ? [0, centroidY] : [2 * centroidY - 28, 28 - centroidY];
-  let croppedImage = pixelArray.slice([imageSliceTop, imageSliceLeft, 0], [2 * ySize, 2 * xSize, 1]);
-  let yPad = (28 - 2*ySize) / 2;
-  let xPad = (28 - 2*xSize) / 2;
-  let paddedImage = croppedImage.pad([[yPad, yPad], [xPad, xPad], [0, 0]]);
+  let paddedImage = preprocessImage(pixelArray);
 
   // run inference
   pixelArray = tf.expandDims(paddedImage, 0); //add batch dimension
   let outputs = ($(".modelbuttons input[id='trained']:checked").val()) ? trainedModel.predict(pixelArray) : untrainedModel.predict(pixelArray);
   return outputs;
 }
+
+function preprocessImage(pixelArray) {
+  let centroidProduct = tf.sum(tf.sum(tf.mul(pixelArray, centroidMap), 1), 0);
+  let pixelSum = tf.sum(tf.sum(pixelArray, 1), 0);
+  let centroid = tf.round(tf.div(centroidProduct, pixelSum));
+  let [centroidY, centroidX] = centroid.dataSync();
+  let [imageSliceLeft, xSize] = (centroidX < 14) ? [0, centroidX] : [2 * centroidX - 28, 28 - centroidX];
+  let [imageSliceTop, ySize] = (centroidY < 14) ? [0, centroidY] : [2 * centroidY - 28, 28 - centroidY];
+  let croppedImage = pixelArray.slice([imageSliceTop, imageSliceLeft, 0], [2 * ySize, 2 * xSize, 1]);
+  let yPad = (28 - 2*ySize) / 2;
+  let xPad = (28 - 2*xSize) / 2;
+  let paddedImage = croppedImage.pad([[yPad, yPad], [xPad, xPad], [0, 0]]);
+  return paddedImage;
+}
+
+// function preprocessImage(pixelArray) {
+//   let centroidProduct = tf.sum(tf.sum(tf.mul(pixelArray, centroidMap), 1), 0);
+//   let pixelSum = tf.sum(tf.sum(pixelArray, 1), 0);
+//   let centroid = tf.round(tf.div(centroidProduct, pixelSum));
+//   let [centroidY, centroidX] = centroid.dataSync();
+//   let top, bottom, left, right;
+
+//   for(let i=0; i<28; i++) {
+//     let rowSlice = pixelArray.slice([i, 0], [1, 28]);
+//     let rowSum = tf.sum(rowSlice);
+//     if (rowSum.dataSync()[0] != 0) {
+//       top = i;
+//       break;
+//     }
+//   }
+//   for(let i=27; i>=0; i--) {
+//     let rowSlice = pixelArray.slice([i, 0], [1, 28]);
+//     let rowSum = tf.sum(rowSlice);
+//     if (rowSum.dataSync()[0] != 0) {
+//       bottom = i;
+//       break;
+//     }
+//   }
+//   for(let i=0; i<28; i++) {
+//     let colSlice = pixelArray.slice([0, i], [28, 1]);
+//     let colSum = tf.sum(colSlice);
+//     if (colSum.dataSync()[0] != 0) {
+//       left = i;
+//       break;
+//     }
+//   }
+//   for(let i=27; i>=0; i--) {
+//     let colSlice = pixelArray.slice([0, i], [28, 1]);
+//     let colSum = tf.sum(colSlice);
+//     if (colSum.dataSync()[0] != 0) {
+//       right = i;
+//       break;
+//     }
+//   }
+
+//   let cropHeight = bottom - top;
+//   let cropWidth = right - left;
+//   // let resizeHeight = 20; //~80% of total height
+//   // let resizeWidth = Math.round(cropWidth / cropHeight * resizeHeight);
+//   // let croppedImage = pixelArray.slice([top, left, 0], [cropHeight, cropWidth, 1]);
+//   // let resizedImage = tf.image.resizeBilinear(pixelArray, [resizeHeight, resizeWidth]);
+
+
+//   let [imageSliceLeft, xSize] = (centroidX < 14) ? [0, centroidX] : [2 * centroidX - 28, 28 - centroidX];
+//   let [imageSliceTop, ySize] = (centroidY < 14) ? [0, centroidY] : [2 * centroidY - 28, 28 - centroidY];
+//   let croppedImage = pixelArray.slice([imageSliceTop, imageSliceLeft, 0], [2 * ySize, 2 * xSize, 1]);
+//   let yPad = (28 - 2*ySize) / 2;
+//   let xPad = (28 - 2*xSize) / 2;
+//   let paddedImage = croppedImage.pad([[yPad, yPad], [xPad, xPad], [0, 0]]);
+//   return paddedImage;
+// }
